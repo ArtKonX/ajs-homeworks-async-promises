adfa6a6f88ed5539d14262f24a185cb944174374
Artemij Kondurov <kondurovartemij@yandex.ru>
2024-08-14 00:09:21 +0300

    Fixed bugs

--- src/js/__tests__/GameSavingLoader.test.js
+++ src/js/__tests__/GameSavingLoader.test.js
@@ -1,17 +1,17 @@
 import GameSavingLoader from "../classes/GameSavingLoader";
 
 test('Тест метода load класса GameSavingLoader', () => {
-    return GameSavingLoader.load().then(saving => expect(JSON.parse(saving)).toEqual({
-        id: 9,
-        created: 1546300800,
-        userInfo: {
-            id: 1, name: 'Hitman', level: 10, points: 2000,
+    const res = JSON.stringify(
+        {
+            id: 9,
+            created: 1546300800,
+            userInfo: {
+                id: 1, name: 'Hitman', level: 10, points: 2000,
+            }
         }
-    }));
-});
+    )
 
-test('Тест если произойдет обещание с ошибкой', () => {
-    return GameSavingLoader.load().catch(error => {
-        expect(error).toThrow();
+    return GameSavingLoader.load().then((saving) => {
+        expect(saving).toEqual(res);
     });
-});
+})
--- src/js/classes/GameSavingLoader.js
+++ src/js/classes/GameSavingLoader.js
@@ -2,8 +2,7 @@ import json from "../modules/parser";
 import read from "../modules/reader";
 
 export default class GameSavingLoader {
-    static async load() {
-        const data = await read();
-        return await json(data);
+    static load() {
+        return read().then((data) => json(data));
     }
 }
