23cc964aeaa0926a8438ad010c033e04311ca0fe
Artemij Kondurov <kondurovartemij@yandex.ru>
2024-08-13 22:29:35 +0300

    Fixed tests

--- src/js/__tests__/GameSavingLoader.test.js
+++ src/js/__tests__/GameSavingLoader.test.js
@@ -1,19 +1,17 @@
 import GameSavingLoader from "../classes/GameSavingLoader";
 
-test('Тест метода load класса GameSavingLoader', (done) => {
-    GameSavingLoader.load().then(saving => expect(JSON.parse(saving)).toEqual({
+test('Тест метода load класса GameSavingLoader', () => {
+    return GameSavingLoader.load().then(saving => expect(JSON.parse(saving)).toEqual({
         id: 9,
         created: 1546300800,
         userInfo: {
             id: 1, name: 'Hitman', level: 10, points: 2000,
         }
     }));
-    done();
 });
 
 test('Тест если произойдет обещание с ошибкой', () => {
-    expect.assertions(1);
     return GameSavingLoader.load().catch(error => {
-        expect.rejects();
+        expect(error).toThrow();
     });
 });
