514bd9a0a9f5a8eecb96ad6b3ae974cc9c5ec2fd
Artemij Kondurov <kondurovartemij@yandex.ru>
2024-08-14 18:27:04 +0300

    Fixed bugs

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,9 @@ import GameSavingLoader from "./classes/GameSavingLoader";
 import GameSaving from "./classes/GameSaving";
 
 GameSavingLoader.load().then((saving) => {
-    const data = JSON.parse(saving)
-    return GameSaving(data)
+    const data = JSON.parse(saving);
+
+    return new GameSaving(data)
 }, (error) => {
     console.error(error)
 });
